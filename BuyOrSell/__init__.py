69d184cbc80ccac35ed0d9c81a910e283ece6ad8
Anmoldeep <43957799+anmoldeep7@users.noreply.github.com>
2021-10-15 14:55:08 +0530

    Update __init__.py

--- BuyOrSell/__init__.py
+++ BuyOrSell/__init__.py
@@ -21,8 +21,8 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = #os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = #os.environ.get('EMAIL_PASS')
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
 
